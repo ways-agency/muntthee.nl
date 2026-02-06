a2bccc337c21b3fcae0abdba5659370f4c736254
Stijn <stijn@code.nl>
2026-02-06 14:52:17 +0100

    Refactor date handling logic in nuxt.config.ts
    - Simplified the assignment of dateCreated and dateModified properties to use nullish coalescing, ensuring they are only set if not already defined.
    - Removed debug console logs to clean up the code and improve maintainability.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,12 +27,8 @@ export default defineNuxtConfig({
         fs.stat(path.join(file.path)),
       );
 
-      console.log("path", file.path);
-      console.log("mtime", mtime);
-      console.log("birthtime", birthtime);
-
-      content.dateCreated = birthtime;
-      content.dateModified = mtime;
+      content.dateCreated = content.dateCreated ?? birthtime;
+      content.dateModified = content.dateModified ?? mtime;
 
       if (!content.draft) {
         content.datePublished = (content.datePublished as Date) ?? mtime;
